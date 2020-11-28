6d783ebc5d354e67204b395d768694c7e9d1b315
kkata <14849535+kkata@users.noreply.github.com>
2020-11-28 17:52:04 +0900

    4. Adding and Editing Tasks

--- src/default-board.js
+++ src/default-board.js
@@ -1,52 +1,52 @@
-import { uuid } from './utils'
+import { uuid } from "./utils";
 
 export default {
-  name: 'workshop',
+  name: "workshop",
   columns: [
     {
-      name: 'todo',
+      name: "todo",
       tasks: [
         {
-          description: '',
-          name: 'first task',
+          description: "",
+          name: "first task",
           id: uuid(),
           userAssigned: null
         },
         {
-          description: '',
-          name: 'second task',
+          description: "",
+          name: "second task",
           id: uuid(),
           userAssigned: null
         },
         {
-          description: '',
-          name: 'and thrid',
+          description: "",
+          name: "and thrid",
           id: uuid(),
           userAssigned: null
         }
       ]
     },
     {
-      name: 'in-progress',
+      name: "in-progress",
       tasks: [
         {
-          description: '',
-          name: 'first task',
+          description: "",
+          name: "first task",
           id: uuid(),
           userAssigned: null
         }
       ]
     },
     {
-      name: 'done',
+      name: "done",
       tasks: [
         {
-          description: '',
-          name: 'first task',
+          description: "",
+          name: "first task",
           id: uuid(),
           userAssigned: null
         }
       ]
     }
   ]
-}
+};
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -20,12 +20,18 @@
               {{ task.name }}
             </span>
             <p
-              v-if="task.descriotion"
+              v-if="task.description"
               class="w-full flex-no-shrink mt-1 text-sm"
             >
               {{ task.description }}
             </p>
           </div>
+          <input
+            type="text"
+            class="block p-2 w-fll bg-transparent"
+            placeholder="+ Enter new task"
+            @keyup.enter="createTask($event, column.tasks)"
+          />
         </div>
       </div>
     </div>
@@ -52,6 +58,13 @@ export default {
     },
     close() {
       this.$router.push({ name: "board" });
+    },
+    createTask(e, tasks) {
+      this.$store.commit("CREATE_TASK", {
+        tasks,
+        name: e.target.value
+      });
+      e.target.value = "";
     }
   }
 };
