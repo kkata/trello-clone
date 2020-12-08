e5ef520d3b9ecc80c3b79a96ed1d314f177bd6ee
kkata <14849535+kkata@users.noreply.github.com>
2020-12-08 19:38:48 +0900

    10. Extracting Drag & Drop

--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -1,26 +1,31 @@
 <template>
-  <div
-    class="task"
-    draggable
-    @dragstart="pickupTask($event, taskIndex, columnIndex)"
-    @click="goToTask(task)"
-    @dragover.prevent
-    @dragenter.prevent
-    @drop.stop="moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)"
-  >
-    <span class="w-full flex-no-shrink font-bold">
-      {{ task.name }}
-    </span>
-    <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
-      {{ task.description }}
-    </p>
-  </div>
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
+      class="task"
+      :transferData="{
+        type: 'task',
+        fromColumnIndex: columnIndex,
+        fromTaskIndex: taskIndex,
+      }"
+      @click.native="goToTask(task)"
+    >
+      <span class="w-full flex-no-shrink font-bold">
+        {{ task.name }}
+      </span>
+      <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+        {{ task.description }}
+      </p>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
+import AppDrag from "./AppDrag";
+import AppDrop from "./AppDrop";
 import movingTasksAndColumnsMixin from "@/mixins/movingTasksAndColumnsMixin";
 
 export default {
+  components: { AppDrag, AppDrop },
   props: {
     task: {
       type: Object,
@@ -33,14 +38,6 @@ export default {
   },
   mixins: [movingTasksAndColumnsMixin],
   methods: {
-    pickupTask(e, taskIndex, fromColumnIndex) {
-      e.dataTransfer.effectAllowed = "move";
-      e.dataTransfer.dropEffect = "move";
-
-      e.dataTransfer.setData("from-task-index", taskIndex);
-      e.dataTransfer.setData("from-column-index", fromColumnIndex);
-      e.dataTransfer.setData("type", "task");
-    },
     goToTask(task) {
       this.$router.push({ name: "task", params: { id: task.id } });
     },
